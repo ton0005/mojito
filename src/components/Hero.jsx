dafa306489f71c9054c996a66619a2a56bba4904
Ton Thi Thanh Truc <ton0005@flinders.edu.au>
2026-01-07 13:34:24 +1030

    Implement Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+const Hero = () => {
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      yPercent: 100,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out",
+      stagger: 0.1,
+      delay: 1,
+    });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right leaf"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic</p>
+              <p className="subtitle">
+                Sip the spirit <br /> of summer
+              </p>
+            </div>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail we serve is a reflection of our obsession with
+                detail — from the first muddle to the final garnish. That care
+                is what turns a simple drink into something truly memorable.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
