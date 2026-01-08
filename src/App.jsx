e4a92ea87cab09494993f1067f526c28c42a1d4c
Ton Thi Thanh Truc <ton0005@flinders.edu.au>
2026-01-08 13:34:37 +1030

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
