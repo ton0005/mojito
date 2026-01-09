2d357a8e5e015041513c7d5829d33b74c2bee8a3
Ton Thi Thanh Truc <ton0005@flinders.edu.au>
2026-01-10 09:52:39 +1030

    Implement About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 const About = () => {
+	useGSAP(() => {
+		const titleSplit = SplitText.create('#about-h2', { type: "words" });
+		const scrollTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: "#about",
+				start: "top center",
+			},
+		});
+		scrollTimeline
+		.from(titleSplit.words, {
+			opacity: 0,
+			yPercent: 100,
+			duration: 1,
+			ease: 'expo.out',
+			stagger: 0.02,
+		})
+		.from('.top-grid div, .bottom-grid div', {
+			opacity: 0,
+			duration: 1,
+			ease: 'power1.inOut',
+			stagger: 0.04,
+		}, "-=0.5");
+	})
 return (
 	<div id="about">
 	 <div className="mb-16 md:px-0 px-5">
